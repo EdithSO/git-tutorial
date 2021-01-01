6c6bcc8ce6354b531875dd50c6aa2b5148214113
EdithSO <67697426+EdithSO@users.noreply.github.com>
2021-01-01 19:02:24 +0100

    clean-up

--- tutorial.py
+++ tutorial.py
@@ -5,17 +5,21 @@ def print_day(d):
 
     days_dict = dict(zip(range(7), days))
     
-    print(days_dict[d])
+    print(f" today is {days_dict[d]}")
 
+def is_weekend(d):
+    if d == 4:
+        print("TGIF!!!")
+    elif current.weekday() in [5,6]:
+        print("WEEKEND!")
+    else:
+        days_till_weekend = 5 - current.weekday()
+        print(f"{days_till_weekend} left to weekend...")
+        
 
-current = datetime.now()
-print_day(current.weekend())
-if current.weekday() == 4:
-    print("TGIF!!!")
-elif current.weekday() in [5,6]:
-    print("WEEKEND!")
-else:
-    days_till_weekend = 5 - current.weekday()
-    print(f"{dats_till_weekend} left to weekend...")
-
+if __name__ == "__main__":
     
+    current = datetime.now()
+    print_day(current.weekend())
+    is_weekend(current.weekend())
+
